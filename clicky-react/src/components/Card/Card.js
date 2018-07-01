8efb0a6ab4b0b90a58f7f66dd41b31e725e9f3cb
Brett Cimbalik <bcimbali@gmail.com>
2018-07-01 11:43:46 -0500

    images grow on hover

--- clicky-react/src/components/Card/Card.js
+++ clicky-react/src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => (
   <div>
-    <img src={props.image} alt={props.name} height="200" width="200" className="img-thumbnail"/>
+    <img src={props.image} alt={props.name} height="200" width="200" className="card-select grow img-thumbnail m-2 pointer"/>
   </div>
 );
 
