5ef9915c882d5f3484f62db1d44b3d0f3234fb1f
Brett Cimbalik <bcimbali@gmail.com>
2018-08-24 23:04:01 -0500

    responsive nav font

--- clicky-react/src/components/Navbar/Navbar.js
+++ clicky-react/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = (props) => (
       <nav className="colors fixed-top navbar nav-height">
         <ul className="row center list-inline nav-fill w-100">
           <li className="col list-inline-item nav-font text-center">
-            <a className="navbar-brand" href="/">Clicky Game</a>
+            <a className="navbar-brand" href="/">Twin Peaks Memory Game</a>
           </li>
           <li className="col list-inline-item my-auto nav-font text-center">
             {props.message}
