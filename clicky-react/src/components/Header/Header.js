cbd06d022c81e6b140148794a243b71e1d564018
Brett Cimbalik <bcimbali@gmail.com>
2018-08-25 13:47:17 -0500

    banner padding responsive

--- clicky-react/src/components/Header/Header.js
+++ clicky-react/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const Header = (props) => (
-  <div className="align-items-center bg-header colors-header jumbotron header-hgt head-mar-top text-center p-0 py-2 yx-auto text-center">
+  <div className="align-items-center bg-header colors-header jumbotron header-hgt header-pad head-mar-top text-center yx-auto text-center">
     <h1 className="header-font" >Click on an image to earn points,</h1>
     <h1 className="header-font">but don't click on any more than once!</h1>
   </div>
