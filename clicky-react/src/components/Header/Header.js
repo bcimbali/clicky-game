35ce6969e5fcbe2652815f4078d90435f5e8ef7d
Brett Cimbalik <bcimbali@gmail.com>
2018-08-25 15:52:34 -0500

    modified bg and theme

--- clicky-react/src/components/Header/Header.js
+++ clicky-react/src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css';
 
 const Header = (props) => (
-  <div className="align-items-center bg-header jumbotron header-hgt header-pad head-mar-top nav-shadow text-center tp-text yx-auto text-center">
-    <h1 className="header-font">CLICK ON AN IMAGE TO EARN POINTS,</h1>
-    <h1 className="header-font">BUT DON'T CLICK ON ANY MORE THAN ONCE!</h1>
+  <div className="align-items-center bg-header jumbotron header-hgt header-pad head-mar-top nav-shadow neon text-center yx-auto text-center">
+    <h1 className="header-font">Click on an image to earn points,</h1>
+    <h1 className="header-font">But don't click on any more than once!</h1>
   </div>
 );
 
