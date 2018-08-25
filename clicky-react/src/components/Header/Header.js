c2b37aa13f4005f24ecbf8cea4fd4e20eb4f5b97
Brett Cimbalik <bcimbali@gmail.com>
2018-08-25 13:20:17 -0500

    responsive nav and header

--- clicky-react/src/components/Header/Header.js
+++ clicky-react/src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css';
 
 const Header = (props) => (
-  <div className="align-items-center bg-header colors-header jumbotron header-hgt text-center mt-4 yx-auto text-center">
-    <h1 className="header-font mt-4" >Click on an image to earn points,</h1>
-    <h1 className="header-font mt-4">but don't click on any more than once!</h1>
+  <div className="align-items-center bg-header colors-header jumbotron header-hgt head-mar-top text-center p-0 py-2 yx-auto text-center">
+    <h1 className="header-font" >Click on an image to earn points,</h1>
+    <h1 className="header-font">but don't click on any more than once!</h1>
   </div>
 );
 
--- clicky-react/src/components/Navbar/Navbar.js
+++ clicky-react/src/components/Navbar/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Navbar = (props) => (
-      <nav className="colors fixed-top navbar nav-height">
-        <ul className="row center list-inline nav-fill w-100">
-          <li className="col list-inline-item nav-font text-center">
-            <a className="navbar-brand" href="/">Twin Peaks Memory Game</a>
+      <nav className="colors fixed-top navbar nav-height nav-shadow p-0">
+        <ul className="row center list-inline m-0 nav-fill nav-height nav-width">
+          <li className="col list-inline-item my-auto nav-calc-font p-0 m-0 text-center">
+            <a className="nav-calc-font navbar-brand p-0 m-0 title-line-hgt" href="/">
+              Twin Peaks <br></br>Memory Game
+            </a>
           </li>
-          <li className="col list-inline-item my-auto nav-font text-center">
+          <li className="col list-inline-item my-auto nav-calc-font m-0 p-0 text-center">
             {props.message}
           </li>
-          <li className="col list-inline-item my-auto nav-font text-center">
+          <li className="col list-inline-item my-auto nav-calc-font m-0 p-0 text-center">
             Score: {props.score} | Top Score: {props.topScore}
           </li>
         </ul>
